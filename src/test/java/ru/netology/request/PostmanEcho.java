a3cc94f64ca01c3f377317164b897fd7dcd17f62
Timur Kiselev <snygens@mail.ru>
2020-08-24 23:29:29 +0300

    add full project

--- src/test/java/ru/netology/request/PostmanEcho.java
+++ src/test/java/ru/netology/request/PostmanEcho.java
@@ -1,4 +1,26 @@
 package ru.netology.request;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PostmanEcho {
+    @Test
+    void shouldPostRequest() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.3 (Java/11.0.7)"));
+    }
 }
