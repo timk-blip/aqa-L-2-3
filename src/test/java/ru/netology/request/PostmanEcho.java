cc352d51b474d670f358cd5b5241a616b6fe1bb0
Timur Kiselev <snygens@mail.ru>
2020-08-25 09:41:49 +0300

    тест падает, разные актуальный и ожидаемый результат

--- src/test/java/ru/netology/request/PostmanEcho.java
+++ src/test/java/ru/netology/request/PostmanEcho.java
@@ -2,17 +2,24 @@ package ru.netology.request;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEcho {
     @Test
     void shouldPostRequest() {
+
+
 // Given - When - Then
 // Предусловия
         given()
+
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .body("Good morning") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
@@ -20,7 +27,6 @@ public class PostmanEcho {
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
-                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.3 (Java/11)"));
+                .body("data", equalTo("Good evening"));
     }
 }
